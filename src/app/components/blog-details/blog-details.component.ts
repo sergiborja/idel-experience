ade1a5aa1734fbb99f6343dbc9bf3fa5ff943cd8
Sergi Borja Gutierrez <sergiborja@MacBook-Pro-de-Sergi.local>
2021-09-27 20:46:57 +0200

    Dinamic experiences routing and starting to add toros

--- src/app/components/blog-details/blog-details.component.ts
+++ src/app/components/blog-details/blog-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     selector: "app-blog-details",
@@ -7,15 +7,29 @@ import { ActivatedRoute } from "@angular/router";
     styleUrls: ["./blog-details.component.scss"],
 })
 export class BlogDetailsComponent implements OnInit {
-    constructor(private route: ActivatedRoute) {}
+    constructor(private route: ActivatedRoute, private router: Router) {}
 
     public expKey: string;
+
+    public experiences = ["trees", "london", "toros"];
 
     ngOnInit() {
         this.expKey = this.route.snapshot.params["expKey"];
     }
 
-    public changeExp(expKey: string) {
-        this.expKey = expKey;
+    public changeExp() {
+        let routeToNavigate: string;
+
+        const idx = this.experiences.findIndex((exp) => this.expKey == exp);
+
+        if (idx == this.experiences.length - 1) {
+            routeToNavigate = this.experiences[0];
+        } else {
+            routeToNavigate = this.experiences[idx + 1];
+        }
+
+        this.router.navigate([`blog-details/${routeToNavigate}`]);
+
+        this.expKey = routeToNavigate;
     }
 }
