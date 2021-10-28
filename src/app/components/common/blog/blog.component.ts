138cd28989e46aadae73d70879e95834864e17b7
Sergi Borja Gutierrez <sergiborja@MacBook-Pro-de-Sergi.local>
2021-10-29 00:32:50 +0200

    [HOT FIX] Added host as script and fixed mobile responsive bugs

--- src/app/components/common/blog/blog.component.ts
+++ src/app/components/common/blog/blog.component.ts
@@ -1,45 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { OwlOptions } from 'ngx-owl-carousel-o';
-import { ViewportScroller } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { OwlOptions } from "ngx-owl-carousel-o";
+import { ViewportScroller } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
-    selector: 'app-blog',
-    templateUrl: './blog.component.html',
-    styleUrls: ['./blog.component.scss']
+    selector: "app-blog",
+    templateUrl: "./blog.component.html",
+    styleUrls: ["./blog.component.scss"],
 })
 export class BlogComponent implements OnInit {
+    constructor(
+        private viewportScroller: ViewportScroller,
+        private router: Router
+    ) {}
 
-    constructor(private viewportScroller: ViewportScroller) {}
-
-    public onClick(elementId: string): void { 
+    public onClick(elementId: string): void {
         this.viewportScroller.scrollToAnchor(elementId);
     }
 
-    ngOnInit() {
-    }
+    ngOnInit() {}
 
     blogSlides: OwlOptions = {
-		loop: true,
-		nav: false,
-		dots: true,
-		autoplayHoverPause: true,
-		autoplay: true,
-		margin: 30,
-		navText: [
-			"<i class='fa fa-angle-left'></i>",
-			"<i class='fa fa-angle-right'></i>"
-		],
-		responsive: {
-			0: {
-				items: 1,
-			},
-			768: {
-				items: 2,
-			},
-			1200: {
-				items: 3,
-			}
-		}
-    }
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplayHoverPause: true,
+        autoplay: false,
+        margin: 30,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>",
+        ],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            768: {
+                items: 2,
+            },
+            1200: {
+                items: 3,
+            },
+        },
+    };
 
+    public nav(route: string) {
+        this.router.navigate([route]);
+    }
 }
